cdaecf70cd79994fe67deee163e26f1118ad50f7
yuhi-sa <yuhisaiki1998@gmail.com>
2020-08-11 13:33:53 +0900

    recommit

--- scripts/listener.py
+++ scripts/listener.py
@@ -2,33 +2,39 @@
 # coding: utf-8
 import rospy
 from std_msgs.msg import String
-#odriveのパッケージを読み込む
+#odriveのパッケージ
 import odrive
+#odriveの初期値などが入っているパッケージ
 from odrive.enums import *
 #その他の利用パッケージ
 import time
-
+import matplotlib.pyplot as plt
 
 #起動しているodriveを見つけて太郎と名付ける
 taro=odrive.find_any()
 print("太郎発見・セットアップします") 
-time.sleep(10)
+time.sleep(3)
 #キャリブレーションする
 taro.axis1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
 time.sleep(20)
 #閉ループ回路に設定する
 taro.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-time.sleep(10)
+time.sleep(5)
 #コントロールモードを指定
 taro.axis1.controller.config.control_mode=CTRL_MODE_VELOCITY_CONTROL
-time.sleep(10)
+time.sleep(5)
 #ポジションゲインを10に設定する
 taro.axis1.controller.config.pos_gain = 10.0
-time.sleep(10)
+time.sleep(3)
+
+#推定位置記録
+est = taro.axis1.encoder.pos_estimate
+plt.figure()
+plt.plot(time.time(),est)
+plt.draw
+
 print("太郎準備完了")
-
 print("現在の制御モードは速度制御")
-
 
 def callback(data):
     #subscriberのID　rospy.get_caller_id()
@@ -52,12 +58,9 @@ def callback(data):
         taro.axis1.controller.vel_setpoint=int(data.data)
     elif mode == 1:
         taro.axis1.controller.pos_setpoint=int(data.data)
-
-    #データの取得
-    #電圧取得
-    #taro.vbus_voltage
-
-
+    
+    #推定値の更新
+    est = taro.axis1.encoder.pos_estimate
 
 def listener():
     #おまじない　ノード名を宣言
